103150b93773ff65bd1181ee1f54cab8a49df9b9
jiangtj <116749895@qq.com>
2017-08-03 09:06:26 +0800

    update pom

--- feign-client-example1/src/main/java/com/jtj/cloud/feignclientexample1/FeignClientExample1Application.java
+++ feign-client-example1/src/main/java/com/jtj/cloud/feignclientexample1/FeignClientExample1Application.java
@@ -2,12 +2,12 @@ package com.jtj.cloud.feignclientexample1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
-@EnableEurekaClient
 @EnableFeignClients
-@SpringBootApplication
+@SpringCloudApplication
 public class FeignClientExample1Application {
 
 	public static void main(String[] args) {
--- feign-client-example2/src/main/java/com/jtj/cloud/feignclientexample1/FeignClientExample2Application.java
+++ feign-client-example2/src/main/java/com/jtj/cloud/feignclientexample1/FeignClientExample2Application.java
@@ -2,14 +2,14 @@ package com.jtj.cloud.feignclientexample1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
-@EnableEurekaClient
 @EnableFeignClients
-@SpringBootApplication
+@SpringCloudApplication
 public class FeignClientExample2Application {
 
 	public static void main(String[] args) {
