94c9980f70af560d1bd5fc5cb5762faacd8f2f2a
jiangtj <116749895@qq.com>
2017-09-15 15:44:52 +0800

    Hystrix Dashboard

--- feign-client-example1/src/main/java/com/jtj/cloud/feignclientexample1/controller/IndexController.java
+++ feign-client-example1/src/main/java/com/jtj/cloud/feignclientexample1/controller/IndexController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
-
 /**
  * Created by MrTT (jiang.taojie@foxmail.com)
  * 2017/8/2.
