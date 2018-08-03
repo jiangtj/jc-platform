39a2ed3ff6cca7530af236883c924dde3c2147a4
jiangtj <116749895@qq.com>
2018-08-03 13:02:57 +0800

    Test config client

--- simple-example/config-client/src/main/java/com/jtj/cloud/configclient/ConfigClientApplication.java
+++ simple-example/config-client/src/main/java/com/jtj/cloud/configclient/ConfigClientApplication.java
@@ -24,7 +24,7 @@ public class ConfigClientApplication {
 
 	@GetMapping("/")
 	public String index(){
-		return "index";
+		return "Config Client Started !!";
 	}
 
 	@GetMapping("/config")
--- simple-example/config-client/src/test/java/com/jtj/cloud/configclient/ConfigClientApplicationTests.java
+++ simple-example/config-client/src/test/java/com/jtj/cloud/configclient/ConfigClientApplicationTests.java
@@ -1,16 +1,42 @@
 package com.jtj.cloud.configclient;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ConfigClientApplicationTests {
 
+	@Autowired
+	private ApplicationContext context;
+
+	private WebTestClient client;
+
+	@Before
+	public void setUp() {
+		client = WebTestClient.bindToApplicationContext(context).build();
+	}
+
 	@Test
 	public void contextLoads() {
+		client.get().uri("/")
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody(String.class).isEqualTo("Config Client Started !!");
+	}
+
+	@Test
+	public void config() {
+		client.get().uri("/config")
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody(String.class).isEqualTo("{\"header\":\"Header\",\"prefix\":\"Prefix\",\"secret\":\"vfdnerjwvgkblhnxcsdceru==\",\"timeout\":\"PT8H\"}");
 	}
 
 }
