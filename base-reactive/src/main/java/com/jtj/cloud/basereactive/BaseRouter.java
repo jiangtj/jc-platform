0c0694a1ae13bb2a514d149f2af85097140dd61b
Mr.J <116749895@qq.com>
2023-03-18 21:47:16 +0800

    .

---
+++ base-reactive/src/main/java/com/jtj/cloud/basereactive/BaseRouter.java
@@ -0,0 +1,26 @@
+package com.jtj.cloud.basereactive;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import com.jtj.cloud.common.BaseExceptionUtils;
+
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
+public class BaseRouter {
+
+	@Bean
+	public RouterFunction<ServerResponse> baseRoutes() {
+		return route()
+				.GET("/insecure/fn/err", serverRequest -> {
+					throw BaseExceptionUtils.invalidToken("fn");
+				})
+				.GET("/fn/needtoken", serverRequest -> {
+					return ServerResponse.ok().bodyValue("ok");
+				})
+				.build();
+	}
+}
