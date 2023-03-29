884d4f3d021892a1d264f8801a567e1c29a1a3d0
Mr.J <116749895@qq.com>
2023-03-29 10:14:06 +0800

    Fix test case

--- micro-auth/src/test/java/com/jtj/cloud/auth/AuthServerApplicationTests.java
+++ micro-auth/src/test/java/com/jtj/cloud/auth/AuthServerApplicationTests.java
@@ -1,9 +1,7 @@
 package com.jtj.cloud.auth;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class AuthServerApplicationTests {
 
     @Test
--- micro-common/src/test/java/com/jtj/cloud/common/MicroCommonApplicationTests.java
+++ micro-common/src/test/java/com/jtj/cloud/common/MicroCommonApplicationTests.java
@@ -1,9 +1,7 @@
 package com.jtj.cloud.common;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class MicroCommonApplicationTests {
 
     @Test
