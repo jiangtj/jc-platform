6677d89fb7f08b2e31a5556f1cb9358113a93525
Mr.J <116749895@qq.com>
2023-05-08 11:03:01 +0800

    add restdocs

--- micro-test/src/main/java/com/jiangtj/cloud/test/JCloudWebTest.java
+++ micro-test/src/main/java/com/jiangtj/cloud/test/JCloudWebTest.java
@@ -1,8 +1,10 @@
 package com.jiangtj.cloud.test;
 
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.lang.annotation.*;
@@ -14,6 +16,8 @@ import java.lang.annotation.*;
 @ExtendWith(JCloudExtension.class)
 @SpringBootTest
 @AutoConfigureWebTestClient
+@AutoConfigureRestDocs
+@Import({JCloudWebTestClientBuilderCustomizerConfiguration.class})
 @ActiveProfiles("testcase")
 public @interface JCloudWebTest {
 }
