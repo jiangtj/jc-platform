7aa44041be29e57b45aa8e7061813977c5b7974c
Mr.J <116749895@qq.com>
2023-09-19 14:03:51 +0800

    fix Lifecycle method

--- micro-common/src/main/java/com/jiangtj/cloud/common/JCloudCommonAutoConfiguration.java
+++ micro-common/src/main/java/com/jiangtj/cloud/common/JCloudCommonAutoConfiguration.java
@@ -3,13 +3,17 @@ package com.jiangtj.cloud.common;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jiangtj.cloud.common.utils.JsonUtils;
 import jakarta.annotation.PostConstruct;
+import jakarta.annotation.Resource;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 
 @AutoConfiguration
 public class JCloudCommonAutoConfiguration {
 
+    @Resource
+    private ObjectMapper mapper;
+
     @PostConstruct
-    public void setup(ObjectMapper mapper) {
+    public void setup() {
         JsonUtils.init(mapper);
     }
 
