286bb996d1758e76fd39380120c233388a1357cb
Mr.J <116749895@qq.com>
2023-09-19 13:28:31 +0800

    add SpelUtils

---
+++ micro-common/src/main/java/com/jiangtj/cloud/common/utils/SpelUtils.java
@@ -0,0 +1,23 @@
+package com.jiangtj.cloud.common.utils;
+
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.spel.support.SimpleEvaluationContext;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+
+public interface SpelUtils {
+
+    static EvaluationContext getMethodContext(Method method, Object[] args) {
+        SimpleEvaluationContext context = SimpleEvaluationContext.forReadOnlyDataBinding().build();
+        context.setVariable("args", args);
+        Parameter[] parameters = method.getParameters();
+        for (int i = 0; i < parameters.length; i++) {
+            Parameter parameter = parameters[i];
+            Object arg = args[i];
+            context.setVariable(parameter.getName(), arg);
+        }
+        return context;
+    }
+
+}
