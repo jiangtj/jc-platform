71505be8ae631120a8d4fef7f97b4518c46428d3
Mr.J <116749895@qq.com>
2023-11-09 17:16:54 +0800

    add IntervalScheduler

---
+++ micro-common/src/test/java/com/jiangtj/cloud/common/reactive/IntervalSchedulerTest.java
@@ -0,0 +1,35 @@
+package com.jiangtj.cloud.common.reactive;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.concurrent.atomic.AtomicInteger;
+
+class IntervalSchedulerTest {
+
+    private IntervalScheduler scheduler;
+
+    @Test
+    public void shouldRunTenTimes() throws InterruptedException {
+        AtomicInteger atomic = new AtomicInteger(0);
+        scheduler = new IntervalScheduler("test", (i) -> {
+            atomic.incrementAndGet();
+            return Mono.empty();
+        }, Duration.ofMillis(100));
+        scheduler.start();
+        Thread.sleep(1099);
+        Assertions.assertEquals(10, atomic.getAcquire());
+    }
+
+
+    @AfterEach
+    public void tearDown() {
+        if (this.scheduler != null) {
+            this.scheduler.stop();
+        }
+    }
+
+}
