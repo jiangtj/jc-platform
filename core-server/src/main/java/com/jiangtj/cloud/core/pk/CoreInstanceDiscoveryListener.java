e4e0d4672fee072d8ba584a8d4c717802038c413
Mr.J <116749895@qq.com>
2023-11-10 16:11:36 +0800

    add micro-common-cloud module for spring cloud utils

--- core-server/src/main/java/com/jiangtj/cloud/core/pk/CoreInstanceDiscoveryListener.java
+++ core-server/src/main/java/com/jiangtj/cloud/core/pk/CoreInstanceDiscoveryListener.java
@@ -1,15 +1,10 @@
 package com.jiangtj.cloud.core.pk;
 
+import com.jiangtj.cloud.common.cloud.InstanceDiscoveryListener;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.discovery.event.HeartbeatEvent;
-import org.springframework.cloud.client.discovery.event.HeartbeatMonitor;
-import org.springframework.cloud.client.discovery.event.InstanceRegisteredEvent;
-import org.springframework.cloud.client.discovery.event.ParentHeartbeatEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
@@ -17,9 +12,7 @@ import java.util.List;
 
 @Slf4j
 @Component
-public class InstanceDiscoveryListener {
-
-    private final HeartbeatMonitor monitor = new HeartbeatMonitor();
+public class CoreInstanceDiscoveryListener extends InstanceDiscoveryListener {
 
     @Resource
     private DiscoveryClient discoveryClient;
@@ -27,33 +20,8 @@ public class InstanceDiscoveryListener {
     @Resource
     private PublicKeyService publicKeyService;
 
-    @EventListener
-    public void onApplicationReady(ApplicationReadyEvent event) {
-        discover();
-    }
 
-    @EventListener
-    public void onInstanceRegistered(InstanceRegisteredEvent<?> event) {
-        discover();
-    }
-
-    @EventListener
-    public void onParentHeartbeat(ParentHeartbeatEvent event) {
-        discoverIfNeeded(event.getValue());
-    }
-
-    @EventListener
-    public void onApplicationEvent(HeartbeatEvent event) {
-        discoverIfNeeded(event.getValue());
-    }
-
-    private void discoverIfNeeded(Object value) {
-        if (this.monitor.update(value)) {
-            discover();
-        }
-    }
-
-    protected void discover() {
+    public void discover() {
         log.info("discovering client change.");
         Flux.fromIterable(discoveryClient.getServices())
             .flatMapIterable(s -> discoveryClient.getInstances(s))
@@ -71,5 +39,4 @@ public class InstanceDiscoveryListener {
             .map(publicKeyService::getCoreServiceInstance)
             .subscribe(publicKeyService::updateCoreServiceInstance);
     }
-
 }
