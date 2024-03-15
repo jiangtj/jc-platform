7bc637c9a60a0883307760dd2bb0cff5afc521ee
Mr.J <116749895@qq.com>
2024-03-15 16:12:15 +0800

    add CoreInstanceApi

--- micro-spring-cloud-starter/src/main/java/com/jiangtj/platform/spring/cloud/ServletAuthKeyLocator.java
+++ micro-spring-cloud-starter/src/main/java/com/jiangtj/platform/spring/cloud/ServletAuthKeyLocator.java
@@ -1,19 +1,13 @@
 package com.jiangtj.platform.spring.cloud;
 
-import com.jiangtj.platform.auth.AuthRequestAttributes;
+import com.jiangtj.platform.spring.cloud.core.CoreInstanceApi;
 import com.jiangtj.platform.spring.cloud.jwt.AuthKeyLocator;
-import com.jiangtj.platform.web.BaseExceptionUtils;
 import io.jsonwebtoken.Header;
 import io.jsonwebtoken.security.Jwks;
 import io.jsonwebtoken.security.PublicJwk;
 import jakarta.annotation.Resource;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.URI;
 import java.security.Key;
 import java.security.PublicKey;
 
@@ -24,6 +18,8 @@ public class ServletAuthKeyLocator implements AuthKeyLocator {
     private CoreInstanceService coreInstanceService;
     @Resource
     private PublicKeyCachedService publicKeyCachedService;
+    @Resource
+    private CoreInstanceApi coreInstanceApi;
 
 
     @Override
@@ -34,17 +30,7 @@ public class ServletAuthKeyLocator implements AuthKeyLocator {
             return publicKeyCachedService.getPublicJwk(kid).toKey();
         }
 
-        URI uri = coreInstanceService.getUri().orElseThrow(() ->
-            BaseExceptionUtils.internalServerError("Core Server isn't running!"));
-
-        String url = String.format("%s/service/%s/publickey", uri, kid);
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(AuthRequestAttributes.TOKEN_HEADER_NAME, coreInstanceService.createToken());
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.add("charset", "utf-8");
-        HttpEntity<Object> entity = new HttpEntity<>(null, headers);
-        String json = restTemplate.exchange(url, HttpMethod.GET, entity, String.class).getBody();
-//        String json = loadBalanced.getForObject(url, String.class);
+        String json = coreInstanceApi.getToken(kid);
         PublicJwk<PublicKey> publicJwk = (PublicJwk<PublicKey>) Jwks.parser().build().parse(json);
 
         publicKeyCachedService.setPublicJwk(publicJwk);
