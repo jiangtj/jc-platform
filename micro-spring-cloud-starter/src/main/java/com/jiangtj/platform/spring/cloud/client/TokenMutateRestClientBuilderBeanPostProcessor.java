8f8975bf277999a4333535d4c112fe728a70d60d
Mr.J <116749895@qq.com>
2024-03-18 15:32:15 +0800

    add tokenMutateInterceptor support in RestClient

--- micro-spring-cloud-starter/src/main/java/com/jiangtj/platform/spring/cloud/client/TokenMutateRestClientBuilderBeanPostProcessor.java
+++ micro-spring-cloud-starter/src/main/java/com/jiangtj/platform/spring/cloud/client/TokenMutateRestClientBuilderBeanPostProcessor.java
@@ -3,22 +3,22 @@ package com.jiangtj.platform.spring.cloud.client;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerInterceptor;
 import org.springframework.context.ApplicationContext;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestClient;
 
 public class TokenMutateRestClientBuilderBeanPostProcessor implements BeanPostProcessor {
 
-	private final ClientHttpRequestInterceptor loadBalancedInterceptor;
+	private final LoadBalancerInterceptor loadBalancerInterceptor;
 	private final TokenMutateHttpRequestInterceptor tokenMutateInterceptor;
 
 	private final ApplicationContext context;
 
 	public TokenMutateRestClientBuilderBeanPostProcessor(
-		ClientHttpRequestInterceptor loadBalancerInterceptor,
+		LoadBalancerInterceptor loadBalancerInterceptor,
 		TokenMutateHttpRequestInterceptor tokenMutateInterceptor,
 		ApplicationContext context) {
-		this.loadBalancedInterceptor = loadBalancerInterceptor;
+		this.loadBalancerInterceptor = loadBalancerInterceptor;
 		this.tokenMutateInterceptor = tokenMutateInterceptor;
 		this.context = context;
 	}
@@ -30,7 +30,7 @@ public class TokenMutateRestClientBuilderBeanPostProcessor implements BeanPostPr
 				return bean;
 			}
 			if (context.findAnnotationOnBean(beanName, LoadBalanced.class) == null) {
-				((RestClient.Builder) bean).requestInterceptor(loadBalancedInterceptor);
+				((RestClient.Builder) bean).requestInterceptor(loadBalancerInterceptor);
 			}
 			((RestClient.Builder) bean).requestInterceptors(c -> c.add(0, tokenMutateInterceptor));
 		}
