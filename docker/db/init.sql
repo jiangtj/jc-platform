b5d84f98cfeb00b6cc4b324833fcaf5d9b5c1959
Mr.J <116749895@qq.com>
2024-12-04 11:13:29 +0800

    build(docker): update MySQL image and configure database settings

--- docker/db/init.sql
+++ docker/db/init.sql
@@ -1,2 +1,2 @@
-create database admin default character set utf8mb4 collate utf8mb4_bin;
-create database core default character set utf8mb4 collate utf8mb4_bin;
+create database `jc_admin` default character set utf8mb4 collate utf8mb4_bin;
+create database `jc_core` default character set utf8mb4 collate utf8mb4_bin;
